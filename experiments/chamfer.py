Fix rotation when rendering chamfer distance

experiments/chamfer.py
@@ -8,11 +8,15 @@ import tensorflow as tf
 
 
 def ins_outs(meshgrid, p):
-    rot = quat2mat(p[-4:])
+    rot = quat2mat(p[8:])
+    # Rotate translation
+    t = quat_product(quat_product(q, p[5:8]+[0]), quat_conjungate(q))
+    # Rotate coordinate system
     m = np.einsum('ij,jabc->iabc', rot, meshgrid)
-    return np.power(np.power((m[0] - p[5]) / p[0], 2 / p[4]) +
-                    np.power((m[1] - p[6]) / p[1], 2 / p[4]), p[4] / p[3]) + \
-           np.power((m[2] - p[7]) / p[2], 2 / p[3])
+    # Calculate
+    return np.power(np.power((m[0] - t[0]) / p[0], 2 / p[4]) +
+                    np.power((m[1] - t[1]) / p[1], 2 / p[4]), p[4] / p[3]) + \
+           np.power((m[2] - t[2]) / p[2], 2 / p[3])
 
 
 def quat2mat(q):
@@ -23,16 +27,30 @@ def quat2mat(q):
          [2*x*z - 2*w*y, 2*y*z + 2*w*x, 1 - 2*(x**2 + y**2)]]
     return np.array(M)
 
+def quat_conjungate(q):
+    return np.array([-q[0], -q[1], -q[2], q[3]])
+
+def quat_product(q1, q2):
+    x1, y1, z1, w1 = q1
+    x2, y2, z2, w2 = q2
+    x = x1 * w2 + y1 * z2 - z1 * y2 + w1 * x2
+    y = -x1 * z2 + y1 * w2 + z1 * x2 + w1 * y2
+    z = x1 * y2 - y1 * x2 + z1 * w2 + w1 * z2
+    w = -x1 * x2 - y1 * y2 - z1 * z2 + w1 * w2
+    return [x, y, z, w]
+
 size = 64
 MESH = np.mgrid[-size/2:size/2, -size/2:size/2, -size/2:size/2].astype(np.float32)
 
-e1, e2 = 1, 1
-a1, a2, a3 = 32, 16, 16
-x, y, z = 0, 0, 0
-q = [0.4082483, 0.4082483, 0.4082483, 0.7071068]
+e = [1, 1]
+a = [32, 16, 16]
+q = [0, 0, 0.7071068, 0.7071068]
+c = [20, 0, 0]
 
 
-params = [a1, a2, a3, e1, e2, x, y, z] + q
+#q = [0, 0, 0, 1]
+
+params = a + e + c + q
 t = time()
 
 